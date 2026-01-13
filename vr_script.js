18451f637c081129e390876ea18e3112ba4c87a8
MRJustYarik <okonechnikov.jarosla@gmail.com>
2026-01-13 14:18:48 +0300

    Hz 01

--- vr_script.js
+++ vr_script.js
@@ -7,12 +7,29 @@ const fetch = require('node-fetch');
 
 const vrChat = new VRChat();
 
+// Просто функция записи в конфиг
 async function writeConfig(tag, value) {
     const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-    config[tag] = value;
+
+    const keys = tag.split('.');
+    let currented = config;
+
+    for (let i = 0; i < keys.length - 1; i++) {
+        if (!currented[keys[i]]) {
+            currented[keys[i]] = {};
+        }
+        currented = currented[keys[i]];
+    }
+
+    currented[keys[keys.length - 1]] = value;
     fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
+
+    if (tag === "user.cookie") {
+        user.cookie = value;
+    }
 }
 
+// Запоминание куки после входа, локально
 async function updateCooke(url) {
     if (url !== user.cookie) {
         writeConfig("user.cookie", url);
@@ -20,7 +37,7 @@ async function updateCooke(url) {
 }
 
 // Взять инфо о группе
-async function getGroupInfo(group_id) {
+async function showGroupInfo(group_id) {
     let group;
     try {
         group = await vrChat.GroupsApi.GetGroupById({
@@ -33,21 +50,46 @@ async function getGroupInfo(group_id) {
     return group;
 }
 
-// Создание файла в api
+// Вывод списка друзей
+async function getListFrends(status) {
+    try {
+        const response = await vrChat.FriendsApi.ListFriends({
+            n: 100,
+            offline: status
+        });
+        const firends = response.map(firend => firend.displayName);
+        if (firends.length == 0) { return "[-]" }
+        return firends;
+    }
+    catch (error) {
+        console.error("ошибка при получении списка друзей", error)
+    }
+}
+
+// Создание файла в api, в разработке ---------------------------------------
+// Мне неиронка предлалала решения, но во первый я их не понимаю, а во вторых 
+// не могу проверитьт, по этому я не стал решение неиронки добавлять сюда.
+
+// По итогу я просто не понимаю куда эту ссылку вставить чтобы он ее загрузил.
+// Типа обьек создается, а куда ссылку кидать я не знаю, тут скорее проблема в 
+// том что я не знаю как работает работа с загрузкой данных
 async function CreateFile(url, name, type, extension) {
     try {
-        // --- БЕСПОЛЕЗНО --------------------------------------------------- 
+        // --- Не используется ---------------------------------------------- 
         const response = await fetch(url);
         const buffer_file = await response.arrayBuffer();
         console.log("Файл скачан, размер:", buffer_file.byteLength, "bytes");
         // ------------------------------------------------------------------
 
+        // Создаем обьект 'файла'
         const obj_file = await vrChat.FilesApi.CreateFile({
             name: name,
             mimeType: type,
             extension: extension
         })
 
+        // Я так понял заливаем 'файл' на API
+        // Но обьект пустой по сути, в нем даже ссылки нет
         const file = await vrChat.FilesApi.CreateFileVersion({
             fileId: obj_file.id,
             signatureMd5: "-",
@@ -55,12 +97,29 @@ async function CreateFile(url, name, type, extension) {
             fileMd5: "-",
             fileSizeInBytes: "-"
         })
-        //console.log(file)
         return file;
     } catch (error) {
         console.error("Ошибка:", error);
     }
 }
+async function addImage(image_url) {
+    const vr_file = await CreateFile(image_url, "test", "image/webp", ".webp")
+
+    let work;
+    try {
+        console.log("groupId:", tfn.id);
+        console.log("groupGalleryId:", tfn.gallery.photo.id);
+        console.log("fileId:", vr_file.id);
+        work = await vrChat.GroupsApi.AddGroupGalleryImage({
+            groupId: tfn.id,
+            groupGalleryId: tfn.gallery.photo.id,
+            fileId: vr_file.id
+        })
+    } catch (error) {
+        console.log("ошибка загрузки в галерею", error)
+    }
+}
+// ---------------------------------------------------------------------------------------
 
 // Любой ввод на основе promise будет работать при условии, что он возвращает код двухфакторной аутентификации.
 function Prompt(query) {
@@ -88,13 +147,12 @@ const asyncMethod = async () => {
         authCookie: user.cookie,
         twoFactorAuth: "",
     }, twoFactor);
-    // Сохраняем Куки если нужно
+    // Сохраняем Куки, локально
     updateCooke(auth.authCookie);
-    //console.log(auth.json.presence);
-
-    // Базовое использование EventsApi с использованием класса VRChat.
+    //console.log("---- Полная аунтификация -------\n", auth)
     vrChat.EventsApi.Connect()
 
+    // ---- Вот это все я точно не знаю зачем, но я это не трогал ----------
     vrChat.EventsApi.on("undocumented event", (data) => {
         console.log("Undocumented Event");
         console.log(data);
@@ -113,29 +171,24 @@ const asyncMethod = async () => {
         console.log("User offline");
         console.log(data);
     })
+    // ---- Вызов функций работы с API ------------------------------------
 
-    //const group = await getGroupInfo(tfn.id)
-
-    console.log("--------------------------------");
-    const image_url = "https://media.discordapp.net/attachments/1439632941847154840/1442642428090650726/Owl.PNG?ex=692d6d35&is=692c1bb5&hm=6a24e845b129cdab90a88b2d7c9935254bbc6a82a59884a16939f59e3ddfa48d&=&format=webp&quality=lossless";
-    const vr_file = await CreateFile(image_url, "test", "image/webp", ".webp")
-
-    let work;
-    try {
-        console.log("groupId:", tfn.id);
-        console.log("groupGalleryId:", tfn.gallery.photo.id);
-        console.log("fileId:", vr_file.id);
-        work = await vrChat.GroupsApi.AddGroupGalleryImage({
-            groupId: tfn.id,
-            groupGalleryId: tfn.gallery.photo.id,
-            fileId: vr_file.id
-        })
-    } catch (error) {
-        console.log("ошибка загрузки в галерею", error)
-    }
+    const frends_offline = await getListFrends(true);
+    const frends_online = await getListFrends(false);
+    const group = await showGroupInfo(tfn.id);
+    console.log("---- Друзья оффлайн ------------\n", frends_offline)
+    console.log("---- Друзья онлайн -------------\n", frends_online)
+    console.log("---- Группа TFN ----------------\n", group)
 
 
-    // -- Optionally disconnect from the API
+    // Изображение из дс, можно свое вставить если изображения нет
+    // В разработке...
+    const image_url = "https://media.discordapp.net/attachments/1439632941847154840/1442642428090650726/Owl.PNG?ex=6966c675&is=696574f5&hm=939c279dee6fbd72c4c3d0039dcd9382400de650ceba45074affbd6aeb138b75&=&format=webp&quality=lossless";
+    //addImage(image_url)
+
+    // --------------------------------------------------------------------
+
+    // При необходимости отсоединить от API
     //vrChat.EventsApi.Disconnect();
 }
 asyncMethod();
